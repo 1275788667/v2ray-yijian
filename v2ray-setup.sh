7248f3f8b4878775f5e8dc0fe6f29ac3d4174774
1275788667 <58962460+1275788667@users.noreply.github.com>
2021-11-04 11:11:02 +0800

    Create v2ray-setup.sh

---
+++ v2ray-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "1
+3
+8888
+
+
+
+" | bash <(curl -s -L https://git.io/v2rayinstall.sh)
+firewall-cmd --zone=public --add-port=1-8079/tcp --permanent
+firewall-cmd --zone=public --add-port=8081-65535/tcp --permanent
+systemctl restart firewalld
+v2ray url
