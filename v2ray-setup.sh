03f0c1004bd83832f65050dfd2006621fdfee310
1275788667 <58962460+1275788667@users.noreply.github.com>
2021-12-03 15:32:12 +0800

    Update v2ray-setup.sh

--- v2ray-setup.sh
+++ v2ray-setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$*${none}; }
+_green() { echo -e ${green}$*${none}; }
+_yellow() { echo -e ${yellow}$*${none}; }
+_magenta() { echo -e ${magenta}$*${none}; }
+_cyan() { echo -e ${cyan}$*${none}; }
 echo "1
 3
 443
@@ -11,3 +22,4 @@ firewall-cmd --zone=public --add-port=1-8079/tcp --permanent
 firewall-cmd --zone=public --add-port=8081-65535/tcp --permanent
 systemctl restart firewalld
 v2ray url
+echo -e "${yellow}网站建设 V2ray 代理等网络相关问题咨询QQ: ${cyan}1275788667${none}"
