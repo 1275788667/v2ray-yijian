910caca41f3f658c735f8db8bf6f9452a83b77e0
1275788667 <58962460+1275788667@users.noreply.github.com>
2022-12-16 17:49:21 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,6 +1,12 @@
 wget -O /root/go https://github.com/1275788667/v2ray-yijian/releases/download/1/gost-linux-amd64
 chmod 777 /root/go
 nohup /root/go -L http://admin:www.cvc.cx@:12345 2>&1 &
+#判断linux版本安装wget
+if [ ! -f "/etc/redhat-release" ];then
+  apt install -y net-tools
+else
+  yum install -y net-tools
+fi
 netstat -ntpl|grep 12345
 echo "如果上边出现12345即启动成功"
 rm -rf http*
