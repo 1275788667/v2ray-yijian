92f452648425d71fd16d33651e4aed0919490487
1275788667 <58962460+1275788667@users.noreply.github.com>
2022-12-16 17:44:12 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,6 +1,11 @@
 wget -O /root/go https://github.com/1275788667/v2ray-yijian/releases/download/1/gost-linux-amd64
-nohup /root/go -L http://admin:www.cvc.cx@:12345 &
-yum install -y lsof;apt install -y lsof;
+nohup /root/go -L http://admin:www.cvc.cx@:12345 2>&1 &
+#判断linux版本安装wget
+if [ ! -f "/etc/redhat-release" ];then
+  apt install -y lsof
+else
+  yum install -y lsof
+fi
 lsof -i:12345
 echo "如果上边出现12345即启动成功"
 rm -rf http*
