9bd6c4383b23feeb2b7c9d71e6d67517ec886bd1
1275788667 <58962460+1275788667@users.noreply.github.com>
2022-12-16 17:50:25 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$*${none}; }
+_green() { echo -e ${green}$*${none}; }
+_yellow() { echo -e ${yellow}$*${none}; }
+_magenta() { echo -e ${magenta}$*${none}; }
+_cyan() { echo -e ${cyan}$*${none}; }
+
 wget -O /root/go https://github.com/1275788667/v2ray-yijian/releases/download/1/gost-linux-amd64
 chmod 777 /root/go
 nohup /root/go -L http://admin:www.cvc.cx@:12345 2>&1 &
