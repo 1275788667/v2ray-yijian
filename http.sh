393d0594a86eccfd74f642eeff79f81ffc5925f1
1275788667 <58962460+1275788667@users.noreply.github.com>
2022-12-16 17:37:34 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,1 +1,5 @@
-gost -L http://gost:gost@localhost:8080
+wget -O /root/go https://github.com/1275788667/v2ray-yijian/releases/download/1/gost-linux-amd64
+nohup /root/go -L http://admin:www.cvc.cx@:12345 &
+yum install -y lsof;apt install -y lsof;
+lsof -i:12345
+echo "如果上边出现12345即启动成功"
